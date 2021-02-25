368b6a5b9000014505c7941ff0810e37ce903133
mayank <mayankece155@gmail.com>
2021-02-26 01:24:02 +0530

    Public and private routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss';
+import { Switch } from 'react-router';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
-    <>
-  <p>Mayank Sharma</p>
-  </>
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
