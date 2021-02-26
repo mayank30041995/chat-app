016cc2c629ac2f6e0bd9c332ff5b6d74f34ce433
mayank <mayankece155@gmail.com>
2021-02-26 23:48:16 +0530

    added signin page and signin with facebook  and google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,14 +1,17 @@
-import firebase from './firebase/app'
-
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBExMMW5EBEyoN96ZHQlTuiG7v8GGVWQOs",
-    authDomain: "chat-web-app-8c050.firebaseapp.com",
-    databaseURL: "https://chat-web-app-8c050-default-rtdb.firebaseio.com",
-    projectId: "chat-web-app-8c050",
-    storageBucket: "chat-web-app-8c050.appspot.com",
-    messagingSenderId: "697554662549",
-    appId: "1:697554662549:web:4163667eb646de3badd79c"
-  };
+  apiKey: 'AIzaSyBExMMW5EBEyoN96ZHQlTuiG7v8GGVWQOs',
+  authDomain: 'chat-web-app-8c050.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-8c050-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-8c050',
+  storageBucket: 'chat-web-app-8c050.appspot.com',
+  messagingSenderId: '697554662549',
+  appId: '1:697554662549:web:4163667eb646de3badd79c',
+};
 
-const app=firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+export const auth = app.auth();
+export const database = app.database();
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,8 +1,58 @@
 import React from 'react';
-
+import firebase from 'firebase/app';
+import { Col, Container, Grid, Icon, Panel, Row, Button, Alert } from 'rsuite';
+import { auth, database } from '../misc/firebase';
 const SignIn = () => {
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
 
-    return <div>sign in</div>;
-}
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      Alert.success('Signed in', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  const onFacebookSignIn = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
+  };
+
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to chart</h2>
+                <p>Progressive chat plateform for neophytes</p>
+              </div>
+
+              <div className="mt-3">
+                <Button block color="blue">
+                  <Icon icon="facebook" /> Continue With Facebook
+                </Button>
+
+                <Button block color="green" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Continue With Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
+};
 
 export default SignIn;
