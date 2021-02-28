894ce063ca47796863024be781c669b571eaa842
mayank <mayankece155@gmail.com>
2021-02-28 15:40:38 +0530

    started to work with chat room list

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -5,7 +5,6 @@ import { useProfile } from '../context/profile.context';
 
 const PrivateRoute = ({ children, ...routeProps }) => {
   const { profile, isLoading } = useProfile();
-  console.log(profile);
   if (isLoading && !profile) {
     return (
       <Container>
