9b1001066d588b4bcab4de1609015ce5a139d278
mayank <mayankece155@gmail.com>
2021-03-03 15:55:50 +0530

    create current room context with usecontextselector

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router';
 import { Loader } from 'rsuite';
-
+import ChatTop from '../../components/chat-window/top';
 import ChatBottom from '../../components/chat-window/bottom';
 import Messages from '../../components/chat-window/messages';
 import { useRooms } from '../../context/rooms.context';
+import { CurrentRoomProvider } from '../../context/current-room.context';
 
 const Chat = () => {
   const { chatId } = useParams();
@@ -20,16 +21,25 @@ const Chat = () => {
   if (!currentRoom) {
     return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
   }
+  const { name, description } = currentRoom;
+
+  const currentRoomData = {
+    name,
+    description,
+  };
 
   return (
-    <>
+    <CurrentRoomProvider data={currentRoomData}>
+      <div className="chat-middle">
+        <ChatTop />
+      </div>
       <div className="chat-middle">
         <Messages />
       </div>
       <div className="chat-bottom">
         <ChatBottom />
       </div>
-    </>
+    </CurrentRoomProvider>
   );
 };
 
